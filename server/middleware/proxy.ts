68113f73c0689d164e79ed0d1419add5f21220bc
VinenTik <tikoovinay7@gmail.com>
2026-05-01 16:53:14 +0530

    build14

---
+++ server/middleware/proxy.ts
@@ -0,0 +1,10 @@
+export default defineEventHandler(async (event) => {
+    const url = event.path
+
+    if (url.startsWith('/api/')) {
+        const backendUrl = 'https://backend-service-703709491101.us-central1.run.app'
+        const targetPath = url.replace('/api', '')
+
+        return proxyRequest(event, `${backendUrl}${targetPath}`)
+    }
+})
